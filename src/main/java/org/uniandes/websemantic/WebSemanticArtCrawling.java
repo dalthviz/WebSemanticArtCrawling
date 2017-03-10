78ad7036623fa0734aa6af207203f56357132b11
dalthviz <d.althviz10@uniandes.edu.co>
2017-03-10 07:21:56 -0500

    Crawler inicial para artcyclopedia.

--- src/main/java/org/uniandes/websemantic/WebSemanticArtCrawling.java
+++ src/main/java/org/uniandes/websemantic/WebSemanticArtCrawling.java
@@ -1,11 +1,13 @@
 package org.uniandes.websemantic;
 
+import main.java.org.uniandes.websemantic.page.Artcyclopedia;
 import org.uniandes.websemantic.page.Artnet;
 
 public class WebSemanticArtCrawling {
 
 	public static void main(String[] args) {
-		Artnet.crawling();
+		//Artnet.crawling();
+                Artcyclopedia.crawling();
     }
 
 	
